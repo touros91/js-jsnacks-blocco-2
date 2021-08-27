d02498047bb9f981a60656d78edb07de35a4611b
touros91 <andreageraci91@gmail.com>
2021-08-27 11:42:56 +0200

    Versione completa con il ciclo while

--- snack-1/main.js
+++ snack-1/main.js
@@ -5,15 +5,34 @@
 
 // versione con il for
 
+// var somma = 0;
+
+// for (var i = 0; i < 5; i++) {
+//     var numeroUtente = parseInt(prompt("Inserisci un numero"));
+//     if (!isNaN (numeroUtente)) {
+//         somma += numeroUtente;
+//     } else {
+//         alert("Devi inserire solo dei numeri!");
+//     }
+// }
+
+// document.getElementById("somma").innerHTML += somma;
+
+
+
+// versione con il while
+
+var i = 0;
 var somma = 0;
 
-for (var i = 0; i < 5; i++) {
+while (i < 5) {
     var numeroUtente = parseInt(prompt("Inserisci un numero"));
     if (!isNaN (numeroUtente)) {
         somma += numeroUtente;
     } else {
         alert("Devi inserire solo dei numeri!");
     }
+    i++;
 }
 
 document.getElementById("somma").innerHTML += somma;
