d7a800bc74c5acf31d92e52daf838752218dbd68
touros91 <andreageraci91@gmail.com>
2021-08-27 13:17:12 +0200

    Refactoring

--- snack-3/main.js
+++ snack-3/main.js
@@ -1,7 +1,6 @@
 // Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una
 // lista di cognomi, Gatsby vuole generare una falsa lista di 3 invitati.
 
-
 var nomi = ["Andrea", "Claudio", "Chiara"];
 
 var cognomi = ["Geraci", "Rossi", "Parisi"];
@@ -11,8 +10,8 @@ var invitati = [];
 // versione col for
 
 for (var i = 0; i < 3; i++) {
-    var nomeCasuale = Math.floor(Math.random(i) * 3);
-    var cognomeCasuale = Math.floor(Math.random(i) * 3);
+    var nomeCasuale = Math.floor(Math.random(i) * nomi.length);
+    var cognomeCasuale = Math.floor(Math.random(i) * cognomi.length);
     var invitato = nomi[nomeCasuale] + " " + cognomi[cognomeCasuale];
     invitati.push(invitato);
 }
