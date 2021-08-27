ade121d0171f6d4774360eda53c406e3e5eddfba
touros91 <andreageraci91@gmail.com>
2021-08-27 12:20:09 +0200

    Aggiunto esercizio snack-2

---
+++ snack-2/main.js
@@ -0,0 +1,11 @@
+// Inserisci un numero, se è pari stampa il numero,
+// se è dispari stampa il numero successivo
+
+
+var numero = parseInt(prompt("Inserisci un numero"));
+
+if (numero % 2 == 0) {
+    console.log(numero);
+} else {
+    console.log(numero + 1);
+}
